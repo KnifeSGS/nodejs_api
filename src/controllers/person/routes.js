c55d79a4934a32fd57026672e2b89d5309f94c53
KnifeSGS <knife.sgs@gmail.com>
2023-02-04 23:16:24 +0100

    api - put, post

---
+++ src/controllers/person/routes.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const data = require('./data')
+
+const controller = express.Router()
+
+controller.get('/', (req, res) => {
+  res.json(data)
+})
+
+// Create a new person
+controller.post('/', (req, res) => {
+  const newPerson = req.body
+  newPerson.id = data[data.length - 1] + 1
+  data.push(newPerson)
+
+  res.status(201)
+  res.json(newPerson)
+})
+
+// Update a person
+controller.put('/:id', (req, res) => {
+  const id = req.params.id
+  const index = data.findIndex(p => p.id === Number(id))
+  const { first_name, last_name, email } = req.body
+
+  data[index] = {
+    id,
+    first_name,
+    last_name,
+    email
+  }
+
+  res.status(200)
+  res.json(data[index])
+})
+
+module.exports = controller
