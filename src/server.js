40d22f03e156c4405b524dedf348bdf34aab2ab7
KnifeSGS <knife.sgs@gmail.com>
2023-02-08 22:02:13 +0100

    jwt

--- src/server.js
+++ src/server.js
@@ -9,6 +9,11 @@ const logger = require('./config/logger')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
+// Authentication
+const authenticateJwt = require('./auth/authenticate')
+const adminOnly = require('./auth/adminOnly')
+const authHandler = require('./auth/authHandler')
+
 const swaggerDocument = YAML.load('./docs/swagger.yaml')
 
 const { username, password, host } = config.get('database')
@@ -25,15 +30,18 @@ mongoose
   })
 
 app.use(morgan('combined', { stream: logger.stream }))
-
 // statikus mappák beállítása, pl képek eléréséhez ha a gyökérben van
 // app.use('/images', express.static('images'))
 // statikus mappák beállítása, pl képek, js, egyéb eléréséhez a public mappából
 app.use(express.static('public'))
-
 app.use(bodyParser.json())
-app.use('/person', require('./controllers/person/person.routes'))
-app.use('/post', require('./controllers/post/post.routes'))
+
+// Router
+app.post('/login', authHandler.login)
+app.post('/refresh', authHandler.refresh)
+app.post('/logout', authHandler.logout)
+app.use('/person', authenticateJwt, require('./controllers/person/person.routes'))
+app.use('/post', authenticateJwt, adminOnly, require('./controllers/post/post.routes'))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use((err, req, res, next) => {
